7af881e72edc7faaed39d70498b681ada79cba3e
Jonas Sciangula Street <joni2back@gmail.com>
2020-10-29 22:21:17 -0300

    Updating strict types for hacktobeerfest

--- src/Client.php
+++ src/Client.php
@@ -8,37 +8,37 @@ final class Client
 {
     private Service $service;
 
-    public function __construct(string $username, string $accessToken, string $apiRoot = null)
+    public function __construct(string $username, string $accessToken, string $apiRoot = null): void
     {
         $this->service = new Service($username, $accessToken, $apiRoot);
     }
 
-    public function bookings()
+    public function bookings(): Resources\Bookings
     {
         return new Resources\Bookings($this->service);
     }
 
-    public function externalSites()
+    public function externalSites(): Resources\ExternalSites
     {
         return new Resources\ExternalSites($this->service);
     }
 
-    public function guests()
+    public function guests(): Resources\Guests
     {
         return new Resources\Guests($this->service);
     }
 
-    public function properties()
+    public function properties(): Resources\Properties
     {
         return new Resources\Properties($this->service);
     }
 
-    public function quotes()
+    public function quotes(): Resources\Quotes
     {
         return new Resources\Quotes($this->service);
     }
 
-    public function users()
+    public function users(): Resources\Users
     {
         return new Resources\Users($this->service);
     }
